c140f976181f84cc739774152cfaadf784e9c883
kushal dave <kushaldave2011@gmail.com>
2020-07-14 18:29:45 +0530

    Initial Commit

---
+++ ML with iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ML with iOS
+//
+//  Created by kushal dave on 14/07/20.
+//  Copyright © 2020 kushal dave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ML with iOSTests/ML_with_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  ML_with_iOSTests.swift
+//  ML with iOSTests
+//
+//  Created by kushal dave on 14/07/20.
+//  Copyright © 2020 kushal dave. All rights reserved.
+//
+
+import XCTest
+@testable import ML_with_iOS
+
+class ML_with_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
